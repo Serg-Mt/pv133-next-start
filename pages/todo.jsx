059ae34474d52ab26f05efd27c7501d73b246a35
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-09-27 20:16:20 +0400

    начало занятия

--- pages/todo.jsx
+++ pages/todo.jsx
@@ -1,5 +1,9 @@
 import ToDoApp from '../components/todo';
+import ToDoAppDelegation from '../components/todo-delegation';
 
 export default function ToDoPage() {
-  return <ToDoApp />;
+  return <>
+    <ToDoApp />;
+    <ToDoAppDelegation />
+  </>;
 }
